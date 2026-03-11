b61a2cb808e7283fe045ab054d8d3e0303a880e8
César <cesasanc@student.hive.fi>
2026-03-11 18:00:09 +0200

    Now uses SQLALCHEMY DB from env

--- alembic/env.py
+++ alembic/env.py
@@ -2,13 +2,12 @@
 
 from __future__ import with_statement
 
+import os
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
-
-from app.core.config import settings
-from app.db.base import Base
+from sqlalchemy import MetaData
 
 config = context.config
 
@@ -16,12 +15,12 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 # Prefer runtime settings, fallback to alembic.ini
-sqlalchemy_url = settings.SQLALCHEMY_DATABASE_URI
+sqlalchemy_url = os.getenv("SQLALCHEMY_DATABASE_URI")
 if sqlalchemy_url:
     config.set_main_option("sqlalchemy.url", sqlalchemy_url)
 
-# Metadata for autogenerate
-target_metadata = Base.metadata
+# Metadata for autogenerate. This project currently has no SQLAlchemy models module.
+target_metadata = MetaData()
 
 
 def run_migrations_offline() -> None:
